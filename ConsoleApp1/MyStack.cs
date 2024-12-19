6028c13e47b04a78b04c334fd3eac5fdf2d3d2eb
yongqing2004 <yqing_l@yahoo.com>
2024-12-18 22:14:36 -0500

    my test project

---
+++ ConsoleApp1/MyStack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MyStack<T>
+    {
+        private List<T> _stackList; // Internal list to hold stack items
+
+        // Constructor to initialize the stack
+        public MyStack()
+        {
+            _stackList = new List<T>();
+        }
+
+        // Push method to add an element to the stack
+        public void Push(T item)
+        {
+            _stackList.Add(item);
+        }
+
+        // Pop method to remove and return the top element from the stack
+        public T Pop()
+        {
+            if (_stackList.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            T item = _stackList[_stackList.Count - 1];
+            _stackList.RemoveAt(_stackList.Count - 1);
+            return item;
+        }
+
+        // Peek method to view the top element of the stack without removing it
+        public T Peek()
+        {
+            if (_stackList.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            return _stackList[_stackList.Count - 1];
+        }
+
+        // Method to check if the stack is empty
+        public bool IsEmpty()
+        {
+            return _stackList.Count == 0;
+        }
+
+        // Method to get the count of elements in the stack
+        public int Count()
+        {
+            return _stackList.Count;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            myStack.Push(10);
+            myStack.Push(20);
+
+            myStack.Pop();
+
+            int x = myStack.Pop();
+
+            Console.WriteLine($"Item(s) left in my stack is: {x}");
+
+        }
+    }
+}
